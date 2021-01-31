5dc1afe6a35df5dcad87bab2a1d7173c92761e9c
Kayboku5000 <62546159+Kayboku5000@users.noreply.github.com>
2021-02-01 08:56:39 +1100

    Create if-v3.py

---
+++ if-v3.py
@@ -0,0 +1,14 @@
+x = 10
+if x < 2:
+    print ('small')
+elif x < 10:
+    print ('medium')
+elif x < 20:
+    print ('bigger')
+elif x < 30:
+    print ('biggerrrr')
+elif x < 40:
+    print ('bigggggger')
+else :
+    print ('large')
+print('all done')
