8ad436d9242ae8a7c6c62094f913eee7c7bb1ecf
Kayboku5000 <62546159+Kayboku5000@users.noreply.github.com>
2021-02-01 08:56:36 +1100

    Create if-v2.py

---
+++ if-v2.py
@@ -0,0 +1,13 @@
+x = 5
+print ('Before 5')
+
+if x== 5: 
+    print ('is 5')
+    print ('is still 5')
+    print ('Third 5')
+print ('Afterwards 5')
+print ('Before 6')
+if x == 6:
+    print ('x equals 6')
+
+print ('finito')
