0c508f09dfc5d7730b4973dcdb1beb6633b21061
Kayboku5000 <62546159+Kayboku5000@users.noreply.github.com>
2021-02-01 08:57:24 +1100

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,19 @@
+
+import time
+sum = 1
+for i in range(1, 466):
+    sum = sum * i
+    time.sleep(0.1)
+    print(sum)
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print("total", sum)
+#print(greet(), "Glenn")
